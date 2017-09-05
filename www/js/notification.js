8121c4d2ddec1e12c926d6f7768fdbe0c1ed7b76
shakibania <shakiba@gmail.com>
2017-09-05 19:57:51 +0430

    0

--- www/js/notification.js
+++ www/js/notification.js
@@ -20,14 +20,14 @@ function register_notification_home() {
 
 		push.on('registration', function(data) {
 			// data.registrationId
-			alert("registration event: " + data.registrationId);
+			//alert("registration event: " + data.registrationId);
 			$.ajax({ type: "POST",
 					url: "http://dlsmgroup.ir/temp/robin/RegisterDeviceIDtoDB.php", 
 					data: {regID : data.registrationId,user:window.localStorage.getItem('uuid'),OS:device.platform},
 					async: false,
 					success : function(text)
 					{
-						alert(text);
+						//alert(text);
 						//last_articles_version = text;
 						console.log('SMGROUP ::::::::::::::::::::::::::::::::::::    Notification registration text : ' + text);
 						window.localStorage.setItem('register_for_notifs','yes');
